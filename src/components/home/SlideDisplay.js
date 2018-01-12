56bb4b40b3fac5ed67435d0d87c621425c921ded
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-01-11 18:30:34 -0600

    boton

--- src/components/home/SlideDisplay.js
+++ src/components/home/SlideDisplay.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import nerd from '../../assets/nerd_pag.png';
+import {Link} from 'react-router-dom';
 
 export const SlideDisplay = () => {
 
@@ -10,7 +11,9 @@ export const SlideDisplay = () => {
                    <h1>Aprende los mejores lenguajes y frameworks de desarrollo </h1>
                    <p>Los mejores Bootcamps y Cursos Online</p>
                     <br/>
-                    <button className="btn_start">Empieza a Aprender</button>
+                    <Link to="/bootcamp">
+                        <button className="btn_start">Empieza a Aprender</button>
+                    </Link>
                </div>
                <div className="slide_img">
                     <img className="pre" src={nerd} alt=""/>
